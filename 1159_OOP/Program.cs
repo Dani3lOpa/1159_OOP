59e8a4c8ec50b4a2ff72278832f581b946731fe7
Daniel Opa <1159@adc.itgymnazium.cz>
2024-02-29 11:26:08 +0100

    init repo

---
+++ 1159_OOP/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace _1159_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public class BankAccount
+        {
+            private int _balance;
+            private string _preNumber;
+            private string accountNumber;
+            private int _bankCode;
+
+
+            public BankAccount(int balance, string preNumber, string accountNumber, int bankCode, string bank)
+            {
+                _balance = balance;
+                _preNumber = preNumber;
+                this.accountNumber = accountNumber;
+                _bankCode = bankCode;
+                Bank = bank;
+            }
+            
+            public void FullAccountName(string accountNumber)
+            {
+                Console.WriteLine(accountNumber);
+                Console.WriteLine(_bankCode);
+            }
+        
+
+            public string Bank { get; set; }
+
+            public void deposit(int amount) 
+            {
+                _balance = _balance + amount;
+            }
+
+            public int withdrawl(int amount)
+            {
+                
+                if (_balance < -1000)
+                {
+                    Console.WriteLine("transaction failed");
+                }
+                _balance = _balance - amount;
+                
+                return _balance;
+                
+
+            }
+            
+
+        }
+    }
+}
